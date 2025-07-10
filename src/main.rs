ce176acd88a16e8359a62e0e1906416d8d49bb0d
José Matos <joser.fmatos03@gmail.com>
2025-07-10 18:55:46 +0100

    Persistent PONG response

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 #![allow(unused_imports)]
-use std::io::{self, Write};
+use std::io::{self, Read, Write};
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
+                println!("New connection established!");
+                let mut buffer = [0; 1024];
+                loop  {
+                    let bytes_read = stream.read(&mut buffer).unwrap();
+                    if bytes_read == 0 {
+                        println!("Connection closed by client.");
+                        break;
+                    }
+
+                    stream.write(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
